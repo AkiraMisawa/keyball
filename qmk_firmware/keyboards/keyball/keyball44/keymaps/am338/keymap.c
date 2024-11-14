91eafd09ae6796bb04a18a5b78de7aec86683de4
Effective Misawa <ccccccccandy@gmail.com>
2024-11-14 23:25:23 +0000

    update keyball44

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/am338/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/am338/keymap.c
@@ -31,43 +31,45 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // 【SCRL_DVI: 0x5DAD】スクロール除数を１つ上げます(max D7 = 1/128)← 最もスクロール遅い
 // 【SCRL_DVD: 0x5DAE】スクロール除数を１つ下げます(min D0 = 1/1)← 最もスクロール速い
 
-////////////////////////////////////
-///
-/// 自動マウスレイヤーの実装 ここから
-/// 参考にさせていただいたページ
-/// https://zenn.dev/takashicompany/articles/69b87160cda4b9
-///
-////////////////////////////////////
-
 enum custom_keycodes
 {
-  KC_MY_BTN1 = KEYBALL_SAFE_RANGE, // Remap上では 0x5DAF
-  KC_MY_BTN2,                      // Remap上では 0x5DB0
-  KC_MY_BTN3                       // Remap上では 0x5DB1
+  KC_MY_BTN1 = KEYBALL_SAFE_RANGE,
+  KC_MY_BTN2,
+  KC_MY_BTN3,
+  KC_MY_BTN4,
+  KC_MY_BTN5,
+  KC_MY_SCR,
+  KC_DOUBLE_CLICK_BTN1,
+  KC_TRIPLE_CLICK_BTN1,
+
 };
 
 enum click_state
 {
   NONE = 0,
-  WAITING,   // マウスレイヤーが有効になるのを待つ。 Wait for mouse layer to activate.
-  CLICKABLE, // マウスレイヤー有効になりクリック入力が取れる。 Mouse layer is enabled to take click input.
-  CLICKING,  // クリック中。 Clicking.
+  WAITING,
+  CLICKABLE,
+  CLICKING,
+  CLICKED,
+  SCROLLING
 };
 
-enum click_state state; // 現在のクリック入力受付の状態 Current click input reception status
-uint16_t click_timer;   // タイマー。状態に応じて時間で判定する。 Timer. Time to determine the state of the system.
+enum click_state state;
+uint16_t click_timer;
 
-uint16_t to_reset_time = 800; // この秒数(千分の一秒)、CLICKABLE状態ならクリックレイヤーが無効になる。 For this number of seconds (milliseconds), the click layer is disabled if in CLICKABLE state.
+uint16_t to_reset_time = 800;
 
-const int16_t to_clickable_movement = 0; // クリックレイヤーが有効になるしきい値
-const uint16_t click_layer = 6;          // マウス入力が可能になった際に有効になるレイヤー。Layers enabled when mouse input is enabled
+uint16_t clicked_stay_time = 150;
+uint16_t clickable_stay_time = 1400;
 
-int16_t mouse_record_threshold = 30; // ポインターの動きを一時的に記録するフレーム数。 Number of frames in which the pointer movement is temporarily recorded.
-int16_t mouse_move_count_ratio = 5;  // ポインターの動きを再生する際の移動フレームの係数。 The coefficient of the moving frame when replaying the pointer movement.
+const int16_t to_clickable_movement = 0;
+const uint16_t click_layer = 6;
+
+int16_t mouse_record_threshold = 30;
+int16_t mouse_move_count_ratio = 5;
 
 int16_t mouse_movement;
 
-// クリック用のレイヤーを有効にする。　Enable layers for clicks
 void enable_click_layer(void)
 {
   layer_on(click_layer);
@@ -75,14 +77,81 @@ void enable_click_layer(void)
   state = CLICKABLE;
 }
 
-// クリック用のレイヤーを無効にする。 Disable layers for clicks.
 void disable_click_layer(void)
 {
   state = NONE;
   layer_off(click_layer);
 }
 
-// 自前の絶対数を返す関数。 Functions that return absolute numbers.
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report)
+{
+  int16_t current_x = mouse_report.x;
+  int16_t current_y = mouse_report.y;
+
+  if (current_x != 0 || current_y != 0) {
+    switch (state)
+    {
+      case WAITING:
+        mouse_movement += my_abs(current_x) + my_abs(current_y);
+
+        if (mouse_movement >= to_clickable_movement)
+        {
+          mouse_movement = 0;
+          enable_click_layer();
+        }
+        break;
+
+      case CLICKABLE:
+        click_timer = timer_read();
+        break;
+
+      case CLICKING:
+        break;
+
+      default:
+        click_timer = timer_read();
+        state = WAITING;
+        mouse_movement = 0;
+    }
+  } else {
+    switch (state)
+    {
+      case WAITING:
+        if (timer_elapsed(click_timer) > 50)
+        {
+          mouse_movement = 0;
+          state = NONE;
+        }
+        break;
+
+      case CLICKABLE:
+        if (timer_elapsed(click_timer) > clickable_stay_time)
+        {
+          disable_click_layer();
+        }
+        break;
+
+      case CLICKING:
+        break;
+
+      case CLICKED:
+        if (timer_elapsed(click_timer) > clicked_stay_time)
+        {
+          disable_click_layer();
+        }
+        break;
+
+      default:
+        mouse_movement = 0;
+        state = NONE;
+    }
+  }
+  mouse_report.x = current_x;
+  mouse_report.y = current_y;
+
+  return mouse_report;
+}
+
 int16_t my_abs(int16_t num)
 {
   if (num < 0)
@@ -93,7 +162,6 @@ int16_t my_abs(int16_t num)
   return num;
 }
 
-// 自前の符号を返す関数。 Function to return the sign.
 int16_t mmouse_move_y_sign(int16_t num)
 {
   if (num < 0)
@@ -104,7 +172,6 @@ int16_t mmouse_move_y_sign(int16_t num)
   return 1;
 }
 
-// 現在クリックが可能な状態か。 Is it currently clickable?
 bool is_clickable_mode(void)
 {
   return state == CLICKABLE || state == CLICKING;
@@ -112,45 +179,199 @@ bool is_clickable_mode(void)
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
+  bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  current_keycode = keycode;
+  bool mod_pressed = (get_mods() != 0);
 
-  switch (keycode)
-  {
-  case KC_MY_BTN1:
-  case KC_MY_BTN2:
-  case KC_MY_BTN3:
-  {
-    report_mouse_t currentReport = pointing_device_get_report();
+  static bool is_gui_active = false;
+  static bool is_ctrl_active = false;
 
-    // どこのビットを対象にするか。 Which bits are to be targeted?
-    uint8_t btn = 1 << (keycode - KC_MY_BTN1);
+  static bool is_lt1_pressed = false;
+  static bool is_lt2_pressed = false;
+  static bool is_lt3_pressed = false;
 
-    if (record->event.pressed)
+  static bool is_kana = false;
+
+  switch (keycode) {
+    case KC_MS_BTN1:
+    case KC_MS_BTN2:
+    case KC_MS_BTN3:
+    case KC_MS_BTN4:
+    case KC_MS_BTN5:
     {
-      // ビットORは演算子の左辺と右辺の同じ位置にあるビットを比較して、両方のビットのどちらかが「1」の場合に「1」にします。
-      // Bit OR compares bits in the same position on the left and right sides of the operator and sets them to "1" if either of both bits is "1".
-      currentReport.buttons |= btn;
-      state = CLICKING;
-    }
-    else
-    {
-      // ビットANDは演算子の左辺と右辺の同じ位置にあるビットを比較して、両方のビットが共に「1」の場合だけ「1」にします。
-      // Bit AND compares the bits in the same position on the left and right sides of the operator and sets them to "1" only if both bits are "1" together.
-      currentReport.buttons &= ~btn;
-      enable_click_layer();
+      if (click_layer && get_highest_layer(layer_state) == click_layer)
+      {
+        if (record->event.pressed)
+        {
+          state = CLICKING;
+        }
+        else
+        {
+          enable_click_layer();
+          state = CLICKED;
+        }
+      }
+      return true;
     }
 
-    pointing_device_set_report(currentReport);
-    pointing_device_send();
-    return false;
+    case KC_LALT:
+    case KC_LSFT:
+    case KC_LCTL:
+    {
+      return true;
+    }
+
+    case LT(1, KC_LANG2):
+      if (record->event.pressed)
+      {
+        click_timer = timer_read();
+        is_lt1_pressed = true;
+        if (keycode == LT(1, KC_LANG2))
+        {
+          is_lt1_lang2_pressed = true;
+        }
+        layer_on(1);
+        disable_click_layer();
+
+        if (is_lt2_pressed)
+        {
+          layer_off(2);
+        }
+        else if (is_lt3_pressed)
+        {
+          layer_off(3);
+        }
+      }
+      else
+      {
+        is_lt1_pressed = false;
+        if (keycode == LT(1, KC_LANG2))
+        {
+          is_lt1_lang2_pressed = false;
+        }
+
+        if (!is_lt1_lang2_pressed && !is_lt1_lang1_pressed)
+        {
+          layer_off(1);
+        }
+        if (is_lt2_pressed)
+        {
+          layer_on(2);
+        }
+        else if (is_lt3_pressed)
+        {
+          layer_on(3);
+        }
+
+        if (timer_elapsed(click_timer) < TAPPING_TERM)
+        {
+          if (keycode == LT(1, KC_LANG2))
+          {
+            tap_code(KC_LANG2);
+            is_kana = false;
+          }
+        }
+
+        if (is_gui_active)
+        {
+          unregister_code(KC_LGUI);
+          is_gui_active = false;
+        }
+      }
+      return false;
+
+    case LT(2, KC_SPC):
+      if (record->event.pressed)
+      {
+        click_timer = timer_read();
+        is_lt2_pressed = true;
+        layer_on(2);
+        disable_click_layer();
+
+        if (is_lt1_pressed)
+        {
+          layer_off(1);
+        }
+        else if (is_lt3_pressed)
+        {
+          layer_off(3);
+        }
+      }
+      else
+      {
+        is_lt2_pressed = false;
+        layer_off(2);
+
+        if (is_lt1_pressed)
+        {
+          layer_on(1);
+        }
+        else if (is_lt3_pressed)
+        {
+          layer_on(3);
+        }
+
+        if (timer_elapsed(click_timer) < TAPPING_TERM)
+        {
+          if (keycode == LT(2, KC_SPC))
+          {
+            tap_code(KC_V);
+          }
+        }
+      }
+      return false;
+
+    case LT(3, KC_ESC):
+    case LT(3, KC_F):
+      if (record->event.pressed)
+      {
+        click_timer = timer_read();
+        is_lt3_pressed = true;
+        layer_on(3);
+        disable_click_layer();
+
+        if (is_lt1_pressed)
+        {
+          layer_off(1);
+        }
+        else if (is_lt2_pressed)
+        {
+          layer_off(2);
+        }
+      }
+      else
+      {
+        is_lt3_pressed = false;
+        layer_off(3);
+
+        if (is_lt1_pressed)
+        {
+          layer_on(1);
+        }
+        else if (is_lt2_pressed)
+        {
+          layer_on(2);
+        }
+
+        if (timer_elapsed(click_timer) < TAPPING_TERM)
+        {
+          tap_code(KC_ESC);
+        }
+
+        if (is_ctrl_active)
+        {
+          unregister_code(KC_LCTRL);
+          is_ctrl_active = false;
+        }
+      }
+      return false;
+
+    default:
+      if (record->event.pressed)
+      {
+        disable_click_layer();
+      }
   }
-
-  default:
-    if (record->event.pressed)
-    {
-      disable_click_layer();
-    }
-  }
-
   return true;
 }
 
@@ -221,71 +442,63 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report)
   return mouse_report;
 }
 
-////////////////////////////////////
-///
-/// 自動マウスレイヤーの実装 ここまで
-///
-////////////////////////////////////
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
-              KC_LALT,KC_LGUI,LCTL_T(KC_LNG2)     ,LT(1,KC_SPC),LT(3,KC_LNG1),                  KC_BSPC,LT(2,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+    KC_TAB , KC_Q , KC_W , KC_E , KC_R , KC_T ,                     KC_Y, KC_U , KC_I , KC_O , KC_P , KC_BSPC ,
+    KC_RCTL , KC_A , KC_S , KC_D , LT(3, KC_F) , KC_G ,             KC_H , LT(3, KC_J) , KC_K , KC_L , KC_SCLN , KC_ENT ,
+    KC_LSFT , KC_Z , KC_X , KC_C , KC_V , KC_B ,                    KC_N , KC_M , KC_COMM , KC_DOT , KC_MINS , KC_RSFT ,
+    KC_LGUI , KC_LALT , LT(1, KC_LNG2) , LT(2, KC_SPC), KC_LSFT,    KC_BSPC , KC_ENT , _______ , _______ , KC_ESC
   ),
 
   [1] = LAYOUT_universal(
-    SSNP_FRE ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    SSNP_VRT ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
-    SSNP_HOR ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
-                  _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    KC_TAB , LSFT(KC_5) , LSFT(KC_4) , LSFT(KC_7) , LSFT(KC_6) , KC_SLSH ,          LSFT(KC_NUBS) , LSFT(KC_9) , LSFT(KC_0) , LSFT(KC_COMM) , LSFT(KC_DOT) , KC_BSPC ,
+    KC_RCTL , LSFT(KC_QUOT) , KC_NUHS , LSFT(KC_8) , LSFT(KC_EQL) , KC_EQL ,        LSFT(KC_MINS) , LSFT(KC_LBRC) , LSFT(KC_RBRC) , KC_SCLN , LSFT(KC_SCLN) , KC_ENT ,
+    KC_LSFT , KC_GRV , LSFT(KC_NUHS) , LSFT(KC_3) , LSFT(KC_1) , LSFT(KC_SLSH) ,    KC_NUBS , KC_LBRC , KC_RBRC , KC_QUOT , LSFT(KC_2) , KC_RSFT ,
+    _______ , _______ , _______ , _______ , _______ ,                               KC_DEL , _______ , _______ , _______ , _______
   ),
 
   [2] = LAYOUT_universal(
-    _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _______  ,
-    _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
-    _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
-                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
+    KC_TAB , _______ , KC_7 , KC_8 , KC_9 , _______ ,    KC_INS  , KC_HOME , KC_UP , KC_END , _______ , KC_BSPC ,
+    KC_RCTL , _______ , KC_4 , KC_5 , KC_6 , KC_0  ,     KC_PGUP , KC_LEFT , KC_DOWN , KC_RIGHT , KC_PGDN , KC_ENT ,
+    KC_LSFT , KC_0 , KC_1 , KC_2 , KC_3 , KC_0 ,         _______  , _______ , _______ , _______ , _______ , KC_RSFT ,
+    _______ , _______ , _______ , _______ , _______ ,    KC_DEL , _______ , _______ , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-                  QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
+    _______ , _______ , _______ , _______ , _______  , _______ ,    KC_F9 , KC_F10 , KC_F11 , KC_F12 , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______  , _______ ,    _______ , _______ , KC_F5 , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______  , _______ ,    CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
+    _______ , _______ , _______  , _______  , _______ ,             _______  , _______  , _______       , KBC_RST  , QK_BOOT
   ),
 
   [4] = LAYOUT_universal(
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-                  _______  , _______  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ ,              _______ , _______ , _______ , _______ , _______
   ),
 
   [5] = LAYOUT_universal(
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-                  _______  , _______  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ ,              _______ , _______ , _______ , _______ , _______
   ),
 
   [6] = LAYOUT_universal(
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  ,KC_MY_BTN1, _______  ,KC_MY_BTN2, _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
-                  _______  , _______  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ ,    KC_MY_BTN4 , KC_MY_BTN1 , KC_MY_BTN3 , KC_MY_BTN2 , KC_MY_BTN5 ,  ,
+    _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , KC_MY_SCR , _______ , _______ , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ ,              _______ , _______ , _______ , _______ , _______
   )
 };
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state)
 {
-  // レイヤーが1または3の場合、スクロールモードが有効になる
-  keyball_set_scroll_mode(get_highest_layer(state) == 3);
-  // keyball_set_scroll_mode(get_highest_layer(state) == 1 || get_highest_layer(state) == 3);
+  keyball_set_scroll_mode(get_highest_layer(state) == 1 || get_highest_layer(state) == 3);
 
   return state;
 }
@@ -296,10 +509,29 @@ layer_state_t layer_state_set_user(layer_state_t state)
 
 void oledkit_render_info_user(void)
 {
-  keyball_oled_render_keyinfo();
-  keyball_oled_render_ballinfo();
+  keyball_oled_render_keyinfo();   // キー情報を表示
+  keyball_oled_render_ballinfo();  // トラックボール情報を表示
 
   oled_write_P(PSTR("Layer:"), false);
   oled_write(get_u8_str(get_highest_layer(layer_state), ' '), false);
+
+  switch (state) {
+    case WAITING:
+      oled_write_ln_P(PSTR("  WAITING"), false);
+      break;
+    case CLICKABLE:
+      oled_write_ln_P(PSTR("  CLICKABLE"), false);
+      break;
+    case CLICKING:
+      oled_write_ln_P(PSTR("  CLICKING"), false);
+      break;
+    case CLICKED:
+      oled_write_ln_P(PSTR("  CLICKED"), false);
+      break;
+    case NONE:
+      oled_write_ln_P(PSTR("  NONE"), false);
+      break;
+  }
 }
+
 #endif
